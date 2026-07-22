27557025c746f7be72d2ef85f1b14a1fb7b0f7e9
ClozyA <aoxuan233@gmail.com>
2026-07-22 11:50:48 +0800

    feat: 恢复旧站页面入口

--- src/config/navBarConfig.ts
+++ src/config/navBarConfig.ts
@@ -2,5 +2,44 @@ import type { NavBarConfig } from "../types/config";
 import { LinkPreset } from "../types/config";
 
 export const navBarConfig: NavBarConfig = {
-	links: [LinkPreset.Home, LinkPreset.Archive],
+	links: [
+		LinkPreset.Home,
+		LinkPreset.Archive,
+		{
+			name: "My",
+			url: "/content/",
+			icon: "material-symbols:person",
+			children: [
+				{
+					name: "Diary",
+					url: "/diary/",
+					icon: "material-symbols:book",
+				},
+				{
+					name: "Friends",
+					url: "/friends/",
+					icon: "material-symbols:group",
+				},
+			],
+		},
+		{
+			name: "Links",
+			url: "/links/",
+			icon: "material-symbols:link",
+			children: [
+				{
+					name: "GitHub",
+					url: "https://github.com/ClozyA",
+					external: true,
+					icon: "fa7-brands:github",
+				},
+				{
+					name: "Bilibili",
+					url: "https://space.bilibili.com/52391971",
+					external: true,
+					icon: "fa7-brands:bilibili",
+				},
+			],
+		},
+	],
 };
